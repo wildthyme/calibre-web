7851f4666f133119e55d655c37498a8a971b65a4
Iris W <46726098+wildthyme@users.noreply.github.com>
2019-04-09 14:35:38 -0400

    disabled gdrive for covers cos it kill load times

--- cps/helper.py
+++ cps/helper.py
@@ -423,22 +423,24 @@ def delete_book(book, calibrepath, book_format):
 
 
 def get_book_cover(cover_path):
-    if ub.config.config_use_google_drive:
-        try:
-            if not web.is_gdrive_ready():
-                return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
-            path=gd.get_cover_via_gdrive(cover_path)
-            if path:
-                return redirect(path)
-            else:
-                web.app.logger.error(cover_path + '/cover.jpg not found on Google Drive')
-                return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
-        except Exception as e:
-            web.app.logger.error("Error Message: " + e.message)
-            web.app.logger.exception(e)
-            # traceback.print_exc()
-            return send_from_directory(os.path.join(os.path.dirname(__file__), "static"),"generic_cover.jpg")
-    else:
+    # Using Google Drive for covers results in miserable load times.
+    if True:
+    # if ub.config.config_use_google_drive:
+    #     try:
+    #         if not web.is_gdrive_ready():
+    #             return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
+    #         path=gd.get_cover_via_gdrive(cover_path)
+    #         if path:
+    #             return redirect(path)
+    #         else:
+    #             web.app.logger.error(cover_path + '/cover.jpg not found on Google Drive')
+    #             return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
+    #     except Exception as e:
+    #         web.app.logger.error("Error Message: " + e.message)
+    #         web.app.logger.exception(e)
+    #         # traceback.print_exc()
+    #         return send_from_directory(os.path.join(os.path.dirname(__file__), "static"),"generic_cover.jpg")
+    # else:
         return send_from_directory(os.path.join(ub.config.config_calibre_dir, cover_path), "cover.jpg")
 
 
